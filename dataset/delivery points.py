cc9a07b1144cad59a518983e81be3b85d1e44286
Prashanna Raj Pandit <erprp99@gmail.com>
2025-09-12 13:31:10 -0500

    generate D_Points by distribution

--- dataset/delivery points.py
+++ dataset/delivery points.py
@@ -4,37 +4,47 @@ import random
 import pandas as pd
 import osmnx as ox
 
-# Load graph
-G = ox.load_graphml("../chicago_street_network.graphml")
+DISTRIBUTION={
+    "equal":[33,33,34],
+    "high_dominance":[50,25,25],
+    "low_dominance":[25,25,50],
+    "med_dominance":[25,50,25]
+}
 
-# Pick 1000 random nodes as delivery points
-delivery_nodes = random.sample(list(G.nodes()), 1000)
+def generate_delivery_points(sample_size=1000,distribution="equal"):
+    # Load graph
+    G = ox.load_graphml("../chicago_street_network.graphml")
 
-# Convert to dataframe
-delivery_data = []
-for node in delivery_nodes:
-    delivery_data.append({
-        "node_id": node,
-        "lat": G.nodes[node]['y'],
-        "lon": G.nodes[node]['x'],
-        # Assign random priority: 1 = High, 2 = Medium, 3 = Low
-        "priority": random.choice([1, 2, 3])
-    })
+    # Pick 1000 random nodes as delivery points
+    delivery_nodes = random.sample(list(G.nodes()), sample_size)
 
-df = pd.DataFrame(delivery_data)
+    # Convert to dataframe
+    delivery_data = []
+    for node in delivery_nodes:
+        delivery_data.append({
+            "node_id": node,
+            "lat": G.nodes[node]['y'],
+            "lon": G.nodes[node]['x'],
+            # Assign random priority: 1 = High, 2 = Medium, 3 = Low
+            "priority": random.choices([1, 2, 3],weights=DISTRIBUTION[distribution])[0]
+        })
 
-# Save to CSV
-df.to_csv("delivery_points.csv", index=False)
+    df = pd.DataFrame(delivery_data)
 
-# Print sample data
-print("Sample Delivery Points:")
-print(df.head())
+    # Save to CSV
+    df.to_csv("delivery_points.csv", index=False)
 
-# Print statistics
-print("\nDataset Statistics:")
-print(df.describe(include='all'))
+    # Print sample data
+    print("Sample Delivery Points:")
+    print(df.head())
 
-# Priority distribution
-print("\nPriority Distribution:")
-print(df['priority'].value_counts())
+    # Print statistics
+    print("\nDataset Statistics:")
+    print(df.describe(include='all'))
 
+    # Priority distribution
+    print("\nPriority Distribution:")
+    print(df['priority'].value_counts())
+
+# How to use ?
+# generate_delivery_points(sample_size=100,distribution="high_dominance")
